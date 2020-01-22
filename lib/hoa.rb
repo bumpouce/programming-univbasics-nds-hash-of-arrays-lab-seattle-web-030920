6ddfe0333dd380c98777ee999a074d177e3501f6
Christine Bumpous <bumpouce@gmail.com>
2020-01-22 01:44:45 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,7 +6,8 @@ BASE_HOA = {
 
 
 def add_character(show, name)
+  puts "Before add: #{BASE_HOA[show]}" 
   BASE_HOA[show] << name
-  puts BASE_HOA[show]
+  puts "After add: #{BASE_HOA[show]}" 
   # Should return the array of the 'show' argument
 end
